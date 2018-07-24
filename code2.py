a615663f740dbdbdc524709000192205ded7396c
Ivan Roa <31189913+jeisonroa1@users.noreply.github.com>
2018-07-24 16:27:59 -0500

    Codigo final
    Final Code

--- code2.py
+++ code2.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+#------------------------------------------------------------------------------------------------
+#Se importan librerias
 from flask import Flask, request
 import mysql.connector
 from mysql.connector import MySQLConnection, Error
 import json
 
- 
-
 app = Flask(__name__)
-
-def getter():
+#-------------------------------------------------------------------------------------------------
+#Metodos especificos
+def getf():
     try:
         conn = mysql.connector.connect(host='localhost',
                                        database='employees',
@@ -18,7 +18,8 @@ def getter():
                                        password='welcome')
         cursor = conn.cursor()
         if conn.is_connected():
-            cursor.execute("SELECT * FROM users")
+            query = "SELECT * FROM users"
+            cursor.execute(query)
             row = cursor.fetchall()
             cursor.close()
         
@@ -29,7 +30,7 @@ def getter():
         conn.close()
     return (row)    
 
-def setter():
+def putf(data):
     try:
         conn = mysql.connector.connect(host='localhost',
                                        database='employees',
@@ -37,8 +38,9 @@ def setter():
                                        password='welcome')
         cursor = conn.cursor()
         if conn.is_connected():
-            cursor.execute("SELECT * FROM users")
-            row = cursor.fetchall()
+            query ="INSERT INTO employees.users(Name,LastName,Age,Profession,Area,Email,Phone,Address,City,Country,MaritalState) VALUES ('%s', '%s',%d,'%s','%s','%s',%d,'%s','%s','%s','%s')" % (data["Name"],data["LastName"],int(data["Age"]),data["Profession"],data["Area"],data["Email"],int(data["Phone"]),data["Address"],data["City"],data["Country"],data["MaritalState"]) 
+            cursor.execute(query)
+            conn.commit()
             cursor.close()
         
     except Error as e:
@@ -46,23 +48,79 @@ def setter():
  
     finally:
         conn.close()
-    return (row)    
 
+def postf(data):
+    try:
+        conn = mysql.connector.connect(host='localhost',
+                                       database='employees',
+                                       user='root',
+                                       password='welcome')
+        cursor = conn.cursor()
+        if conn.is_connected():
+            query ="UPDATE employees.users SET Age=Age+1 WHERE Name='%s'" % (data["Name"]) 
+            cursor.execute(query)
+            conn.commit()
+            cursor.close()
+        
+    except Error as e:
+        print(e)
+ 
+    finally:
+        conn.close()
+
+def deletef(data):
+    try:
+        conn = mysql.connector.connect(host='localhost',
+                                       database='employees',
+                                       user='root',
+                                       password='welcome')
+        cursor = conn.cursor()
+        if conn.is_connected():
+            query = "DELETE FROM employees.users WHERE Name='%s'" % (data["Name"])  
+            cursor.execute(query)
+            conn.commit()
+            cursor.close()
+        
+    except Error as e:
+        print(e)
+ 
+    finally:
+        conn.close()
+
+
+
+#----------------------------------------------------------------------------------------------
+#Metodos de Flask
 @app.route('/', methods=['GET'])
 def get():
 
     if request.method == "GET":
-        row = getter()
-    return (json.dumps(str(row)))
+        row = getf()
+    return ("Se han mostrado todos los registros"+json.dumps(str(row)))
+
+@app.route('/PUT', methods=['PUT'])
+def put():
+    if request.method == "PUT":
+        data=request.get_json()
+        putf(data)
+        return ("Se ha añadido a"+data["Name"])
 
 @app.route('/POST', methods=['POST'])
 def post():
     if request.method == "POST":
-        print('GET RECIBIDO')
-        datos=request.get_json()
-        name=datos["name"]
-        load()
-        return ("POST")
+        data=request.get_json()
+        print(data)
+        postf(data)
+        return ("Se ha actualizado a"+data["Name"])
 
+@app.route('/DELETE', methods=['DELETE'])
+def delete():
+    if request.method == "DELETE":
+        data=request.get_json()
+        deletef(data)
+        return ("Se ha eliminado "+data["Name"])
+#----------------------------------------------------------------------------------------------
+#Main Principal
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=5000)
+
